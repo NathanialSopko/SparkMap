b19171fba9627ca0c334d215175e39f32dd46e74
Drewwent <drew.wentka@gmail.com>
2017-09-21 22:55:00 -0700

    Firebase Authentication back end support added. Bugfixes

--- app/src/main/java/com/sparkmap/sparkmap/LoginActivity.java
+++ app/src/main/java/com/sparkmap/sparkmap/LoginActivity.java
@@ -137,7 +137,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
-            // form field with an error.
+            // form field with an error..
             focusView.requestFocus();
         } else {
             // Show a progress spinner, and kick off a background task to
